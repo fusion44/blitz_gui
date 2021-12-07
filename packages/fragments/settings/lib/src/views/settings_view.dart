27d16840ab4533db0b43d549c22629cb1876d4d7
Stefan Stammberger <some.fusion@gmail.com>
2021-12-07 19:48:28 +0100

    fix: remove unnecessary widget in settings view

--- packages/fragments/settings/lib/src/views/settings_view.dart
+++ packages/fragments/settings/lib/src/views/settings_view.dart
@@ -9,92 +9,90 @@ class SettingsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const SizedBox(height: 32.0),
-          BlocBuilder<SettingsBloc, SettingsBaseState>(
-            bloc: BlocProvider.of(context),
-            builder: (context, state) {
-              return ListTile(
-                title: const TrText('settings.language_label'),
-                trailing: DropdownButton(
-                  value: state.langCode,
-                  onChanged: (String? value) {
-                    BlocProvider.of<SettingsBloc>(context).add(
-                      ChangeLanguageEvent(value!),
-                    );
-                  },
-                  items: _buildLanguageItems(),
+    return Column(
+      children: [
+        const SizedBox(height: 32.0),
+        BlocBuilder<SettingsBloc, SettingsBaseState>(
+          bloc: BlocProvider.of(context),
+          builder: (context, state) {
+            return ListTile(
+              title: const TrText('settings.language_label'),
+              trailing: DropdownButton(
+                value: state.langCode,
+                onChanged: (String? value) {
+                  BlocProvider.of<SettingsBloc>(context).add(
+                    ChangeLanguageEvent(value!),
+                  );
+                },
+                items: _buildLanguageItems(),
+              ),
+            );
+          },
+        ),
+        BlocBuilder<SettingsBloc, SettingsBaseState>(
+          bloc: BlocProvider.of(context),
+          builder: (context, state) {
+            return ListTile(
+              title: const TrText('settings.theme_label'),
+              trailing: Switch(
+                value: state.darkTheme,
+                onChanged: (value) {
+                  var bloc = BlocProvider.of<SettingsBloc>(context);
+                  bloc.add(ToggleThemeEvent());
+                },
+              ),
+            );
+          },
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 50.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  const snackBar = SnackBar(content: Text('TODO :('));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                style: ElevatedButton.styleFrom(primary: Colors.orange),
+                child: const TrText(
+                  'settings.reboot_button',
+                  overflow: TextOverflow.ellipsis,
+                  isButton: true,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16.0),
+            SizedBox(
+              height: 50.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  const snackBar = SnackBar(content: Text('TODO :('));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                style: ElevatedButton.styleFrom(primary: Colors.red),
+                child: const TrText(
+                  'settings.shutdown_button',
+                  overflow: TextOverflow.ellipsis,
+                  isButton: true,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8.0),
+        TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => const DebugView(),
                 ),
               );
             },
-          ),
-          BlocBuilder<SettingsBloc, SettingsBaseState>(
-            bloc: BlocProvider.of(context),
-            builder: (context, state) {
-              return ListTile(
-                title: const TrText('settings.theme_label'),
-                trailing: Switch(
-                  value: state.darkTheme,
-                  onChanged: (value) {
-                    var bloc = BlocProvider.of<SettingsBloc>(context);
-                    bloc.add(ToggleThemeEvent());
-                  },
-                ),
-              );
-            },
-          ),
-          const Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 50.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    const snackBar = SnackBar(content: Text('TODO :('));
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                  style: ElevatedButton.styleFrom(primary: Colors.orange),
-                  child: const TrText(
-                    'settings.reboot_button',
-                    overflow: TextOverflow.ellipsis,
-                    isButton: true,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 16.0),
-              SizedBox(
-                height: 50.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    const snackBar = SnackBar(content: Text('TODO :('));
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                  style: ElevatedButton.styleFrom(primary: Colors.red),
-                  child: const TrText(
-                    'settings.shutdown_button',
-                    overflow: TextOverflow.ellipsis,
-                    isButton: true,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8.0),
-          TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => const DebugView(),
-                  ),
-                );
-              },
-              child: const Text('DebugView'))
-        ],
-      ),
+            child: const Text('DebugView'))
+      ],
     );
   }
 
