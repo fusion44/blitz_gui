c02eec287f1b71fc129df7ceee9364b58628aaf7
fusion44 <some.fusion@gmail.com>
2023-07-22 21:27:59 +0200

    feat: add a simple ValueRange class

--- packages/common/lib/common.dart
+++ packages/common/lib/common.dart
@@ -2,4 +2,5 @@ library common;
 
 export 'src/blitz_logger.dart';
 export 'src/utils.dart';
+export 'src/value_range.dart';
 export 'src/models/models.dart';
---
+++ packages/common/lib/src/value_range.dart
@@ -0,0 +1,6 @@
+class ValueRange {
+  final int start;
+  final int end;
+
+  const ValueRange(this.start, this.end);
+}
